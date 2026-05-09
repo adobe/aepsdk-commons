4b87cfe082e1ce96f31de95ff64fa480ec42c170
Navratan Soni <navratans@adobe.com>
2026-05-09 15:55:25 +0530

    Changed to compile SDK 36

--- android/aepsdk-gradle-plugin/aepsdk-gradle-plugin/src/main/kotlin/com/adobe/marketing/mobile/gradle/BuildConstants.kt
+++ android/aepsdk-gradle-plugin/aepsdk-gradle-plugin/src/main/kotlin/com/adobe/marketing/mobile/gradle/BuildConstants.kt
@@ -19,8 +19,8 @@ import org.gradle.api.JavaVersion
 object BuildConstants {
     object Versions {
         const val MIN_SDK_VERSION = 21
-        const val COMPILE_SDK_VERSION = 34
-        const val TARGET_SDK_VERSION = 34
+        const val COMPILE_SDK_VERSION = 36
+        const val TARGET_SDK_VERSION = 36
 
         const val VERSION_CODE = 1
         const val VERSION_NAME = "1"
